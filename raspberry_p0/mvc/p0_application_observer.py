f269ea53c97767bfc2d1ef8c606f75c45d742515
Paulo Mateus <mateus.moura@hotmail.com>
2017-05-15 16:30:53 -0300

    Fix #9 - License (#14)

--- raspberry_p0/mvc/p0_application_observer.py
+++ raspberry_p0/mvc/p0_application_observer.py
@@ -1,3 +1,17 @@
+# Copyright 2017 SrMouraSilva
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from application.component.application_observer import ApplicationObserver
 from pluginsmanager.model.update_type import UpdateType
 from raspberry_p0.action.actions_facade import ActionsFacade
@@ -8,8 +22,11 @@ class P0ApplicationObserver(ApplicationObserver):
     def __init__(self, actions):
         super().__init__()
         self.controller = None
-        self.token = ActionsFacade.TOKEN
         self.actions = actions
+
+    @property
+    def token(self):
+        return ActionsFacade.TOKEN
 
     def register(self, controller):
         self.controller = controller
--- test/test.py
+++ test/test.py
@@ -1,11 +1,25 @@
+# Copyright 2017 SrMouraSilva
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from application.application import Application
 from raspberry_p0.raspberry_p0 import RaspberryP0
 
-application = Application(data_pedalboard="data/", address='localhost', test=True)
+application = Application(path_data="data/", address='localhost', test=True)
 
 p0 = RaspberryP0(application, configuration_file='config_test.ini')
 
 application.register(p0)
 application.start()
 
-p0.close()
+application.stop()
