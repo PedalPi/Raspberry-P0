4582dcab07b19fed29c5f9097909f143f6f20f4c
SrMouraSilva <mateus.moura@hotmail.com>
2017-05-17 08:10:04 -0300

    Test fix build

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         'gpiozero',
         'PedalPi-Application',
-        'PedalPi-Physical'
+        'PedalPi-Raspberry-Physical'
     ],
 
     test_suite='test',
